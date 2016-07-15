f6eb166d2ca07dc26874b9b240c6f6e10afe824e
Fagner Brack <github@fagnermartins.com>
2016-07-16 01:36:34 +1000

    Created named functions

--- app/src/token/store-token-no-complexity-no-mocks.js
+++ app/src/token/store-token-no-complexity-no-mocks.js
@@ -3,7 +3,7 @@
 const Promise = require( "bluebird" );
 const apiRequest = require( "./api-request" );
 
-module.exports = function( token ) {
+module.exports = function storeToken( token ) {
   return Promise.try(function() {
     return apiRequest.post( "/store-token", token );
   });
--- app/src/token/store-token.js
+++ app/src/token/store-token.js
@@ -6,7 +6,7 @@ const apiRequest = require( "./api-request" );
 const isValid = require( "./validation/is-valid" );
 const shouldMarkAsInvalid = require( "./invalidation/should-mark-as-invalid" );
 
-module.exports = function( token ) {
+module.exports = function storeToken( token ) {
   return Promise.try(function() {
     if ( !isValid( token ) ) {
       return Promise.reject( new Error( "Token is not valid" ) );
